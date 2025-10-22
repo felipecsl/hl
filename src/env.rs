5ec2e174b18ef2f046958397112561cfb6192003
Felipe Lima <felipe.lima@gmail.com>
2025-10-22 13:35:05 -0700

    minor nits

--- src/env.rs
+++ src/env.rs
@@ -1,7 +1,7 @@
 use anyhow::{Context, Result};
 use std::collections::HashMap;
 
-use crate::config::build_env_file;
+use crate::{config::build_env_file, log::debug};
 
 /// Read environment variable key-value pairs from a .env (or .env.build) file
 /// # Arguments
@@ -12,6 +12,7 @@ pub fn load_env_file_contents(path: &std::path::Path) -> Result<HashMap<String, 
   let mut map = HashMap::new();
   for item in dotenvy::from_path_iter(path)? {
     let (k, v) = item?;
+    debug(&format!("Loaded env var: {}=****", k));
     map.insert(k, v);
   }
   Ok(map)
@@ -25,8 +26,8 @@ pub fn load_env_file_contents(path: &std::path::Path) -> Result<HashMap<String, 
 /// # Notes
 /// If the build environment file does not exist, returns an empty map
 pub fn load_build_env_contents(app: &str) -> Result<HashMap<String, String>> {
-  let build_env_path = build_env_file(app).display().to_string();
-  return if std::path::Path::new(&build_env_path).exists() {
+  let build_env_path = build_env_file(app);
+  return if build_env_file(app).exists() {
     // Optional: warn if perms are too loose
     #[cfg(unix)]
     {
@@ -36,12 +37,13 @@ pub fn load_build_env_contents(app: &str) -> Result<HashMap<String, String>> {
       if mode & 0o077 != 0 {
         eprintln!(
           "warning: {} is group/world-readable (mode {:o}); consider chmod 600",
-          &build_env_path, mode
+          &build_env_path.display(),
+          mode
         );
       }
     }
-    load_env_file_contents(std::path::Path::new(&build_env_path))
-      .with_context(|| format!("Failed to read {}", &build_env_path))
+    load_env_file_contents(&build_env_path)
+      .with_context(|| format!("Failed to read {}", &build_env_path.display()))
   } else {
     Ok(HashMap::new())
   };
